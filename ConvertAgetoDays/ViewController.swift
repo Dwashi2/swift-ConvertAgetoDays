0c92940858ef479ab2d135c22d128063837c78bc
Daniel Washington <danielvertigo@hotmail.com>
2021-06-11 13:25:19 -0300

    Files

--- ConvertAgetoDays/ViewController.swift
+++ ConvertAgetoDays/ViewController.swift
@@ -5,6 +5,23 @@
 //  Created by Daniel Washington Ignacio on 11/06/21.
 //
 
+/*
+ Create a function that takes the age in years and returns the age in days.
+
+ Examples
+
+ calcAge(65) ➞ 23725
+
+ calcAge(0) ➞ 0
+
+ calcAge(20) ➞ 7300
+ Notes
+
+ Use 365 days as the length of a year for this challenge.
+ Ignore leap years and days between last birthday and now.
+ Expect only positive integer inputs.
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,6 +29,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(self.calcAge(65))
+        print(self.calcAge(0))
+        print(self.calcAge(20))
+    }
+    
+    func calcAge(_ age: Int) -> Int {
+        return age*365
     }
 
 
